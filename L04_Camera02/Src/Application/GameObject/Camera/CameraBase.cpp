4dc3a72ca7622f131a84a83e8a393b8207a097ed
fujiyu1343179 <kd1343179@st.kobedenshi.ac.jp>
2024-06-10 11:14:46 +0900

    L04_Camera02のコミット
    トラッキングカメラ

---
+++ L04_Camera02/Src/Application/GameObject/Camera/CameraBase.cpp
@@ -0,0 +1,23 @@
+﻿#include "CameraBase.h"
+
+void CameraBase::Init()
+{
+	if (!m_spCamera)//二重初期化を防ぐ
+	{
+		m_spCamera = std::make_shared<KdCamera>();
+	}
+}
+
+void CameraBase::Update()
+{
+	if (!m_spCamera)return;
+
+	m_spCamera->SetCameraMatrix(m_mWorld);
+}
+
+void CameraBase::PreDraw()
+{
+	if (!m_spCamera)return;
+
+	m_spCamera->SetToShader();
+}
---
+++ L04_Camera02/Src/Application/GameObject/Camera/TrackingCamera/TrackingCamera.cpp
@@ -0,0 +1,28 @@
+﻿#include "TrackingCamera.h"
+
+void TrackingCamera::Init()
+{
+	//基準点として(ターゲット)からどれだけ離れえているか
+	m_LocalPos = Math::Matrix::CreateTranslation(0, 6.f, -5.f);
+
+	//どれだけ傾けているか
+	m_Rotation = Math::Matrix::CreateRotationX(DirectX::XMConvertToRadians(45));
+
+	CameraBase::Init();
+}
+
+void TrackingCamera::Update()
+{
+	//ターゲットの行列(有効な場合使用する)
+	Math::Matrix targetMat = Math::Matrix::Identity;
+
+	const std::shared_ptr<const KdGameObject> spTarget = m_wpTarget.lock();
+	if (spTarget)
+	{
+		targetMat = spTarget->GetMatrix();
+	}
+
+	m_mWorld = m_Rotation * m_LocalPos * targetMat;
+
+	CameraBase::Update();
+}
